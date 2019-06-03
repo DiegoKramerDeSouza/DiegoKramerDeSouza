7aab4208b7f402e779555d87e133260411aa2d81
Diego Kramer de Souza <diego.kramer99@gmail.com>
2019-06-03 16:49:07 -0500

    ASCIIAnimation completed

---
+++ Animations/js/ascii.js
@@ -0,0 +1,53 @@
+'use strict';
+let frames = 250;
+let interval;
+let anime;
+let num = 0;
+let started = false;
+
+const startAnimation = () => {
+    if(!started){
+        started = true;
+        anime = document.querySelector('#panel').value.split('=====\n');
+    }
+    interval = setInterval(() => {
+        document.querySelector('#panel').value = anime[num];
+        num == anime.length - 1 ? num = 0 : num++;
+    }, frames);
+};
+const blankMsg = () => 'Please select an Animation!';
+
+document.querySelector('#panel').innerHTML = ANIMATIONS[document.querySelector('#animation').value];
+
+document.querySelector('#start').onclick = () => {
+    if(document.querySelector('#animation').value == 'blank') {
+        document.querySelector('#panel').innerHTML = blankMsg();
+        return;
+    }
+    document.querySelector('#start').disabled = true;
+    document.querySelector('#stop').disabled = false;
+    startAnimation();
+}
+document.querySelector('#stop').onclick = () => {
+    clearInterval(interval);
+    document.querySelector('#start').disabled = false;
+    document.querySelector('#stop').disabled = true;
+    started = false;
+    document.querySelector('#panel').value = ANIMATIONS[document.querySelector('#animation').value];
+}
+document.querySelector('#animation').onchange = () => {
+    num = 0;
+    document.querySelector('#panel').value = ANIMATIONS[document.querySelector('#animation').value];
+    if(started){
+        anime = document.querySelector('#panel').value.split('=====\n');
+    }
+}
+document.querySelector('#turbo').onchange = () => {
+    frames = document.querySelector('#turbo').checked ? 50 : 250;
+    if(started && interval){
+        clearInterval(interval);
+        startAnimation();
+    }
+}
+document.querySelector('#size').onchange = () => 
+    document.querySelector('#panel').className = document.querySelector('#size').value;
