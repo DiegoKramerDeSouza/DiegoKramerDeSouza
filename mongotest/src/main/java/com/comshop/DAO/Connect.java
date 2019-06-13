4e132e6215633b105ce57bc036414d7dd4c80513
Diego Kramer de Souza <diego.kramer99@gmail.com>
2019-06-13 13:43:53 -0500

    New content

---
+++ mongotest/src/main/java/com/comshop/DAO/Connect.java
@@ -0,0 +1,7 @@
+package com.comshop.DAO;
+
+import com.mongodb.client.MongoClient;
+
+public class Connect {
+    MongoClientURI uri = new MongoClientURI();
+}
---
+++ mongotest/src/main/java/com/comshop/User1.java
@@ -0,0 +1,4 @@
+package com.comshop;
+
+public class User1 {
+}
